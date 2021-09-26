82e01cfd30140566191bf3295350a419520fb679
jet2018 <ezrajet9@gmail.com>
2021-09-26 15:44:11 +0300

    finally done

--- eastalk/settings.py
+++ eastalk/settings.py
@@ -141,11 +141,11 @@ STATICFILES_DIRS = (
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-MEDIA_URL = "/media/"
+# MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
 
 if not DEBUG:
     STATICFILES_DIRS = (
