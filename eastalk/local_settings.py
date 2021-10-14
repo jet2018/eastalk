9189f3afed310669df943e3e9bd4397559188fff
jet2018 <ezrajet9@gmail.com>
2021-10-14 10:36:05 -0700

    altered some db fields

--- eastalk/local_settings.py
+++ eastalk/local_settings.py
@@ -6,12 +6,18 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': env('DB_ENGINE'),
+    #     'NAME': env('DB_NAME'),
+    #     'USER': env('DB_USER'),
+    #     'PASSWORD': env('DB_PASS'),
+    #     'HOST': '127.0.0.1',
+    #     'PORT': env('DB_PORT')
+    # }
     'default': {
-        'ENGINE': env('DB_ENGINE'),
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
-        'HOST': '127.0.0.1',
-        'PORT': env('DB_PORT')
-    }
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
+
+
